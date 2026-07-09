13b3f72bcbac6ec146d2675a481a6d64c4c2c52f
Lavkush003 <lovekushnishad003@gmail.com>
2026-07-09 14:24:42 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (44.93%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -9,26 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-TreeNode* prev=NULL;
+// class Solution {
+// public:
+// TreeNode* prev=NULL;
 
-bool inorder(TreeNode* root){
-    if(root==NULL){
-        return true;
-    }
-    if(!inorder(root->left))
-      return false;
+// bool inorder(TreeNode* root){
+//     if(root==NULL){
+//         return true;
+//     }
+//     if(!inorder(root->left))
+//       return false;
 
-      if(prev!=NULL && prev->val>=root->val)
-      return false;
-      prev=root;
-      return inorder(root->right);
+//       if(prev!=NULL && prev->val>=root->val)
+//       return false;
+//       prev=root;
+//       return inorder(root->right);
 
-}
-    bool isValidBST(TreeNode* root) {
-        return inorder(root);
+// }
+//     bool isValidBST(TreeNode* root) {
+//         return inorder(root);
 
         
+//     }
+// };
+
+
+class Solution {
+public:
+
+    bool check(TreeNode* root, long long minVal, long long maxVal) {
+
+        if (root == NULL)
+            return true;
+
+        if (root->val <= minVal || root->val >= maxVal)
+            return false;
+
+        return check(root->left, minVal, root->val) &&
+               check(root->right, root->val, maxVal);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return check(root, LLONG_MIN, LLONG_MAX);
     }
 };
